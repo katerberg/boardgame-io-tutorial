d77fd63737f494ec12ba13ecc26cb26a1b05badc
Mark Katerberg <katerberg@fastmail.fm>
2022-12-21 11:42:15 -0600

    Allows AI panel work

--- src/Game.js
+++ src/Game.js
@@ -48,4 +48,15 @@ export const TicTacToe = {
       G.cells[id] = playerID;
     },
   },
+  ai: {
+    enumerate: (G, ctx) => {
+      let moves = [];
+      for (let i = 0; i < 9; i++) {
+        if (G.cells[i] === null) {
+          moves.push({ move: "clickCell", args: [i] });
+        }
+      }
+      return moves;
+    },
+  },
 };
